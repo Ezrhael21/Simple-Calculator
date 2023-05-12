9967b007cc37be1bb9c6fbf2a2254fc058690b72
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-05-12 21:07:30 +0800

    Pseudocode

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -5,3 +5,18 @@
 # math operations (Addition, Subtraction, Multiplication and Division). 
 # User will input 2 numbers and calculate it based on the chosen math operators.
 
+# Import tkinter module to create Graphical User Interface
+# Define a function to be called when the button is pressed
+# Retrieve the values from the input fields and operator selection
+# Perform the calculation based on the selected operator
+# Display error when the case is dividing by zero
+# Update the result label with the calculated value
+# Handle the case of invalid inputs
+# Clear the input fields after each calculation
+# Create a new window
+# Create a frame to hold the input fields
+# Add labels and input fields for the two input values
+# Add a label and drop-down menu for selecting the operator
+# Add a button to trigger the calculation
+# Add a label to display the calculated result
+# Start the main event loop to keep the window open
