aec53baf881b935522647e03becbb9271c1eb297
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-05-12 20:56:01 +0800

    First Commit

---
+++ Simple_Calculator.py
@@ -0,0 +1,7 @@
+# Ezrhael R. Sicat
+# BSCpE 1-5
+# 05/12/2023
+# Simple App Calculator: User will choose one of the four 
+# math operations (Addition, Subtraction, Multiplication and Division). 
+# User will input 2 numbers and calculate it based on the chosen math operators.
+
