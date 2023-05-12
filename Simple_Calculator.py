c813107f11f48f1271ed2a2763639d1f4cceb8e4
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-05-12 21:19:33 +0800

    Import tkinter module

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -6,6 +6,7 @@
 # User will input 2 numbers and calculate it based on the chosen math operators.
 
 # Import tkinter module to create Graphical User Interface
+from tkinter import *
 # Create a new window
 # Create a frame to hold the input fields
 # Add labels and input fields for the two input values
